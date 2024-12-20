d56aba0a6975f3834f2834c1779a3097187849c8
Dhara <80586973+dhara1611@users.noreply.github.com>
2024-12-20 16:54:16 +0000

    variables details

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,15 @@
+const EmployeeId = 1234;
+let EmployeeName = "Dhara";
+var EmployeeCity = "Ahmedabad";
+EmployeePassword = "1245632";
+
+
+
+// EmployeeId = 457896; not allowed to modify 
+EmployeeName = "Pooja";
+EmployeeCity = "Bangluru";
+EmployeePassword = "789456";
+
+console.log(EmployeeId);
+console.table([EmployeeName, EmployeeCity,EmployeePassword]);
+
