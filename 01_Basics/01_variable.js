19cacf3852e398ad61d1b2d40e3632a62e60dec3
Dhara <80586973+dhara1611@users.noreply.github.com>
2024-12-21 11:12:13 +0000

    All Datatypes

--- 01_Basics/01_variable.js
+++ 01_Basics/01_variable.js
@@ -6,6 +6,10 @@ EmployeePassword = "1245632";
 
 
 // EmployeeId = 457896; not allowed to modify 
+/*
+not prefre to use Var 
+because of issue of Block scope and Functional Scope
+*/
 EmployeeName = "Pooja";
 EmployeeCity = "Bangluru";
 EmployeePassword = "789456";
---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,29 @@
+let Name="Dhara" //string
+let age = 30; // number 
+let state; //undefined 
+let job = true //boolean
+let salary = null // null it's "typeof" object
+
+alert(3+3) // give error because we write it in node js not on browser engine 
+console.log(3+3); 
+console.log("Dhara") ;
+// code readibility should be must
+
+
+
+// typeof used to find values datatype
+
+
+// DataTypes
+/*
+primitive datatype
+number 
+Bigint
+string
+null
+undefined
+bollean
+
+non primitive datatype
+object
+*/
