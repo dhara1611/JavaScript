d1a317d092f590c855c668ac55eac3cc2234cecc
Dhara <80586973+dhara1611@users.noreply.github.com>
2024-12-21 11:54:46 +0000

    conversion operation on number string and boolean

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+
+// string to number conversion
+let string = undefined;
+
+let vlaueString = Number(string);
+//console.log(vlaueString); //NaN
+//console.log(typeof vlaueString); //number
+//      Value   but it convert in number 
+// "" => 0 
+//"33abc" => NaN
+// null =>  0 
+//undefined => Nan
+
+
+
+
+//number string  to  boolen conversion
+let LogIn = 31;
+
+let valueLogin = Boolean(LogIn);
+// console.log(valueLogin);
+// console.log(typeof valueLogin);
+
+
+//"Dhara" => true
+// 31 => true
+// 0 => false
+// 1 => true
+
+
+// number to String
+ 
+let name = 0;
+
+let valuename = String(name);
+
+console.log(valuename);
+console.log(typeof valuename);
+//33 => 33
+//null => null
+//undefined  => undefined
+// "" => space "empty space "
