64b537b0ff34f7dc293b39debff65b953719a01d
Dhara <80586973+dhara1611@users.noreply.github.com>
2024-12-21 13:27:31 +0000

    opertaions discussed

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -34,9 +34,37 @@ let name = 0;
 
 let valuename = String(name);
 
-console.log(valuename);
-console.log(typeof valuename);
+// console.log(valuename);
+// console.log(typeof valuename);
 //33 => 33
 //null => null
 //undefined  => undefined
 // "" => space "empty space "
+
+
+//****************************** operations *************************************
+// basic arithmatic operator
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello";
+let str2 = " Dhara";
+
+console.log(str1 + str2);
+
+let value = 3;
+let negvalue = -value;
+
+console.log(negvalue);
+
+console.log("1" + 2 );
+console.log(1 + "2" );
+
+console.log(1+3+"2");
+console.log(typeof(1+3+"2"));
+console.log("1"+1+2);
+
+
