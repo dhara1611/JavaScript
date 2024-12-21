16a2f71f7aaa3ff68007209faf001ff5a6aa0317
Dhara <80586973+dhara1611@users.noreply.github.com>
2024-12-21 16:44:23 +0000

    camarision operator

---
+++ 01_Basics/04_camparision.js
@@ -0,0 +1,16 @@
+// comarison between same datatypa 
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+
+// Comprision between differnt data type
+
+console.log(null == 0) //false
+console.log(null > 0) //false
+console.log(null>=0); //true  js convert null into zero
+
+console.log(undefined == 0) //false
+console.log(undefined > 0) //false
+console.log(undefined>=0); //false 
