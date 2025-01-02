e5f39179fb68ee006d3098f797fe7800c7a151f5
Dhara <80586973+dhara1611@users.noreply.github.com>
2025-01-02 15:46:09 +0000

    get object and array data in function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -28,5 +28,33 @@ function loginUserMessage(username="ram"){
 }
 
 console.log(loginUserMessage("Dhara"));
+//rest operator in function
 
- 
+function calculatecartPrice(...num1){// rest operator collect all value in one bunch and store in array
+    return num1
+}
+console.log(calculatecartPrice(100,200,300))
+
+// get object data in function 
+
+
+const User={
+    username : "Dhara",
+    Price:199,
+}
+
+function handaleObject(getData) {
+    return `username is ${getData.username} and product Price is ${getData.Price}`    
+}
+
+console.log(handaleObject(User));
+
+// get Array data in function 
+
+const mynewArray = [200, 400 , 600]
+
+function handaleArray(getArray){
+        return `array value ${getArray[2]}`
+}
+
+console.log(handaleArray(mynewArray))
