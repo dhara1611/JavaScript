891b92961a285c81d0b32c9424eec896304143fc
Dhara <80586973+dhara1611@users.noreply.github.com>
2025-01-18 09:41:26 +0000

    while , do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,27 @@
+let index = 0;
+//  while(index < 5){
+//     console.log(index);
+//     index++;
+//  }
+
+let myArray = ["dhara","priya","riya"]
+
+let arr = 0;
+while (arr<myArray.length) {
+   console.log(myArray[arr])
+   arr++;
+}
+
+
+// do {
+//    console.log(index);
+//     index++;
+   
+// } while (index<5);
+
+// let score = 11
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
