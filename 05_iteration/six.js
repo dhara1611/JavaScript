545edeaa88e41dbab1fe450d5fb0b5a9001347e9
Dhara <80586973+dhara1611@users.noreply.github.com>
2025-01-31 14:12:12 +0000

    for Each loop and filter

---
+++ 05_iteration/six.js
@@ -0,0 +1,41 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// foreach not return value 
+
+// const values = coding.forEach((item)=>{
+//     return console.log(item)
+// })
+// console.log(values);
+
+// filter returns value
+const num = [1,2,3,4,5,6,7,8,9,10]
+// const value = num.filter((item)=> item>5)
+
+// console.log(value);
+
+// const newnum =[];
+// num.forEach((item)=>{
+//     if(item>5){
+//         newnum.push(item)
+//     }
+// })
+// console.log(newnum);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let newedition = books.filter((book)=>{
+        return book.genre==="History" && book.publish >= 1995
+  })
+  console.log(newedition);
+  
